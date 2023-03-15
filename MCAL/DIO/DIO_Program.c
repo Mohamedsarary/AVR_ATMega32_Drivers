89644b11ff3623352a7556859c38af072b1db069
Mohamed Sarary <89752348+Mohamedsarary@users.noreply.github.com>
2023-03-15 16:48:47 +0200

    Add files via upload

---
+++ MCAL/DIO/DIO_Program.c
@@ -0,0 +1,198 @@
+/*
+ *  	Created on: 15/03/2023
+ *      Driver: DIO Driver
+ *      Author: Mohamed sarary
+ *      File  : DIO_Program.c
+ */
+
+#include "../../Services/STD_TYPES/STD_TYPES.h"
+#include "../../Services/BIT_MATH/BIT_MATH.h"
+
+#include "DIO_interface.h";
+#include "DIO_Private.h" ;
+#include "DIO_Confing.h";
+
+void DIO_Set_Port_Dir(u8 Copy_u8Port, u8 Copy_u8Direction) {
+	switch (Copy_u8Port) {
+	case DIO_u8PortA:
+		DDRA = Copy_u8Direction;
+		break;
+	case DIO_u8PortB:
+		DDRB = Copy_u8Direction;
+		break;
+	case DIO_u8PortC:
+		DDRC = Copy_u8Direction;
+		break;
+	case DIO_u8PortD:
+		DDRD = Copy_u8Direction;
+		break;
+	}
+}
+
+void DIO_Set_Port_Val(u8 Copy_u8Port, u8 Copy_u8Value) {
+	switch (Copy_u8Port) {
+	case DIO_u8PortA:
+		PORTA = Copy_u8Value;
+		break;
+	case DIO_u8PortB:
+		PORTB = Copy_u8Value;
+		break;
+	case DIO_u8PortC:
+		PORTC = Copy_u8Value;
+		break;
+	case DIO_u8PortD:
+		PORTD = Copy_u8Value;
+		break;
+	}
+
+}
+
+void DIO_Set_Pin_Dir(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Direction) {
+	switch (Copy_u8Port) {
+	case DIO_u8PortA:
+		switch (Copy_u8Direction) {
+		case DIO_u8_OUTPUT:
+			SET_BIT(DDRA, Copy_u8Pin);
+			break;
+
+		case DIO_u8_INPUT:
+			CLR_BIT(DDRA, Copy_u8Pin);
+			break;
+		}
+		break;
+	case DIO_u8PortB:
+		switch (Copy_u8Direction) {
+		case DIO_u8_OUTPUT:
+			SET_BIT(DDRB, Copy_u8Pin);
+			break;
+
+		case DIO_u8_INPUT:
+			CLR_BIT(DDRB, Copy_u8Pin);
+			break;
+		}
+		;
+		break;
+	case DIO_u8PortC:
+		switch (Copy_u8Direction) {
+		case DIO_u8_OUTPUT:
+			SET_BIT(DDRC, Copy_u8Pin);
+			break;
+
+		case DIO_u8_INPUT:
+			CLR_BIT(DDRC, Copy_u8Pin);
+			break;
+		}
+		;
+		break;
+	case DIO_u8PortD:
+		switch (Copy_u8Direction) {
+		case DIO_u8_OUTPUT:
+			SET_BIT(DDRD, Copy_u8Pin);
+			break;
+
+		case DIO_u8_INPUT:
+			CLR_BIT(DDRD, Copy_u8Pin);
+			break;
+		}
+		;
+		break;
+	}
+
+}
+
+void DIO_Set_Pin_Val(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Value) {
+	switch (Copy_u8Port) {
+	case DIO_u8PortA:
+		switch (Copy_u8Value) {
+		case DIO_u8_HIGH:
+			SET_BIT(PORTA, Copy_u8Pin);
+			break;
+
+		case DIO_u8_LOW:
+			CLR_BIT(PORTA, Copy_u8Pin);
+			break;
+		}
+		break;
+	case DIO_u8PortB:
+		switch (Copy_u8Value) {
+		case DIO_u8_HIGH:
+			SET_BIT(PORTB, Copy_u8Pin);
+			break;
+
+		case DIO_u8_LOW:
+			CLR_BIT(PORTB, Copy_u8Pin);
+			break;
+		}
+		;
+		break;
+	case DIO_u8PortC:
+		switch (Copy_u8Value) {
+		case DIO_u8_HIGH:
+			SET_BIT(PORTC, Copy_u8Pin);
+			break;
+
+		case DIO_u8_LOW:
+			CLR_BIT(PORTC, Copy_u8Pin);
+			break;
+		}
+		;
+		break;
+	case DIO_u8PortD:
+		switch (Copy_u8Value) {
+		case DIO_u8_HIGH:
+			SET_BIT(PORTD, Copy_u8Pin);
+			break;
+
+		case DIO_u8_LOW:
+			CLR_BIT(PORTD, Copy_u8Pin);
+			break;
+		}
+		;
+		break;
+	}
+
+}
+
+u8 DIO_Read_Pin_Val(u8 Copy_u8Port, u8 Copy_u8Pin) {
+	u8 Local_u8_PINSTATE = 0;
+	switch (Copy_u8Port) {
+	case DIO_u8PortA:
+		Local_u8_PINSTATE = GET_BIT(PINA, Copy_u8Pin);
+		break;
+	case DIO_u8PortB:
+		Local_u8_PINSTATE = GET_BIT(PINB, Copy_u8Pin);
+		break;
+	case DIO_u8PortC:
+		Local_u8_PINSTATE = GET_BIT(PINC, Copy_u8Pin);
+		break;
+	case DIO_u8PortD:
+		Local_u8_PINSTATE = GET_BIT(PIND, Copy_u8Pin);
+		break;
+	}
+	return Local_u8_PINSTATE;
+}
+
+void DIO_ACT_PULLUP(u8 Copy_u8Port, u8 Copy_u8Pin) {
+	/*Define PORT As INPUT ( DDRA = 0x00 ) Then Use Register PORT ( PORTA = 0xFF ) so i Actived the PULLUP Resistor of The PORT*/
+
+	switch (Copy_u8Port) {
+	case DIO_u8PortA:
+		CLR_BIT(DDRA, Copy_u8Pin);
+		SET_BIT(PORTA, Copy_u8Pin);
+		break;
+	case DIO_u8PortB:
+		CLR_BIT(DDRB, Copy_u8Pin);
+		SET_BIT(PORTB, Copy_u8Pin);
+		break;
+	case DIO_u8PortC:
+		CLR_BIT(DDRC, Copy_u8Pin);
+		SET_BIT(PORTC, Copy_u8Pin);
+		break;
+	case DIO_u8PortD:
+		CLR_BIT(DDRD, Copy_u8Pin);
+		SET_BIT(PORTD, Copy_u8Pin);
+		break;
+	}
+
+}
+
---
+++ MCAL/DIO/DIO_interface.h
@@ -0,0 +1,54 @@
+/*
+ *  	Created on: 15/03/2023
+ *      Driver: DIO Driver
+ *      Author: Mohamed sarary
+ *      File  : DIO_Interface.h
+ */
+
+#ifndef MCAL_DIO_DIO_INTERFACE_H_
+#define MCAL_DIO_DIO_INTERFACE_H_
+
+#include "../../Services/STD_TYPES/STD_TYPES.h"
+
+
+
+/*i can use Enum instead of it*/
+#define DIO_u8PortA  0
+#define DIO_u8PortB  1
+#define DIO_u8PortC  2
+#define DIO_u8PortD  3
+
+#define DIO_u8_PORT_INPUT   0
+#define DIO_u8_PORT_OUTPUT  0XFF
+
+#define DIO_u8_PORT_LOW   0
+#define DIO_u8_PORT_HIGH  0XFF
+
+#define DIO_u8_INPUT  0
+#define DIO_u8_OUTPUT 1
+
+#define DIO_u8_PIN_00 0
+#define DIO_u8_PIN_01 1
+#define DIO_u8_PIN_02 2
+#define DIO_u8_PIN_03 3
+#define DIO_u8_PIN_04 4
+#define DIO_u8_PIN_05 5
+#define DIO_u8_PIN_06 6
+#define DIO_u8_PIN_07 7
+
+#define DIO_u8_HIGH 1
+#define DIO_u8_LOW  0
+
+void DIO_Set_Port_Dir(u8 Copy_u8Port , u8 Copy_u8Direction);
+
+void DIO_Set_Port_Val(u8 Copy_u8Port , u8 Copy_u8Value);
+
+void DIO_Set_Pin_Dir(u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Direction);
+
+void DIO_Set_Pin_Val(u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Value);
+
+u8 DIO_Read_Pin_Val (u8 Copy_u8Port , u8 Copy_u8Pin );
+
+void DIO_ACT_PULLUP(u8 Copy_u8Port , u8 Copy_u8Pin );
+
+#endif /* MCAL_DIO_DIO_INTERFACE_H_ */
