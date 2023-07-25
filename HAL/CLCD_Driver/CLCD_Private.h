17e38683d90e024b0c1ddff9ca01cf95ae8a1a14
Mohamed Sarary <89752348+Mohamedsarary@users.noreply.github.com>
2023-07-25 16:01:35 +0300

    Add files via upload

--- HAL/CLCD_Driver/CLCD_Private.h
+++ HAL/CLCD_Driver/CLCD_Private.h
@@ -1,21 +1,30 @@
 /*
- *  	Created on: 15/03/2023
+ *  	Created on: 09/07/2023
  *      Driver: CLCD Driver
  *      Author: Mohamed sarary
  *      File  : CLCD_Private.h
  */
 
-#ifndef HAL_CLCD_DRIVER_CLCD_PRIVATE_H_
-#define HAL_CLCD_DRIVER_CLCD_PRIVATE_H_
+#ifndef HAL_CLCD_CLCD_PRIVATE_H_
+#define HAL_CLCD_CLCD_PRIVATE_H_
 
-#define CLCD_u8_SetFun  0b00111100
-#define CLCD_u8_Dis_Ctr 0b00001100
-#define CLCD_u8_ENT_MOD 0b00000110
-#define CLCD_u8_Clear   0b00000001
-#define CLCD_Default_Position 0b10000000
-#define CLCD_u8_DDRM_Base 0x80
-#define CLCD_u8_DDRM_ROW2_OFFSET 64
-#define CLCD_u8_CGRAM_ADD   0x40
-#define CLCD_u8_4DATA_LENGHT  0
-#define CLCD_u8_8DATA_LENGHT  1
-#endif /* HAL_CLCD_DRIVER_CLCD_PRIVATE_H_ */
+
+#define CLCD_u8SET_FUNCTION			0b00111100
+
+#define CLCD_u8DISPLAY_CONTROL		0b00001100
+
+#define CLCD_u8DISPLAY_CLEAR		0b00000001
+
+#define CLCD_u8_ENTRY_MODE			0b00000110
+
+#define CLCD_u8_DDRAM_OFFSET	0x80
+
+#define CLCD_u8_DDRAM_ROW2_OFFSET	64
+
+
+#define CLCD_u8_CGRAM_OFFSET		0x40
+
+#define CLCD_u8_CLEAR_LCD_COMMAND	0b00000001
+
+
+#endif /* HAL_CLCD_CLCD_PRIVATE_H_ */
--- HAL/CLCD_Driver/CLCD_Program.c
+++ HAL/CLCD_Driver/CLCD_Program.c
@@ -1,136 +1,134 @@
 /*
- *  	Created on: 15/03/2023
+ *  	Created on: 09/07/2023
  *      Driver: CLCD Driver
  *      Author: Mohamed sarary
  *      File  : CLCD_Program.c
  */
 
-#include "../../MCAL/DIO/DIO_interface.h"
-#include "CLCD_Confing.h"
-#include "CLCD_Private.h"
+#include "CLCD_Config.h"
 #include "CLCD_Interface.h"
-#include <util/delay.h>
+#include "CLCD_Private.h"
+#include "../../SERVICES/BIT_MATH.h"
+#include "../../SERVICES/STD_TYPES.h"
+#include "../../MCAL/DIO/DIO_interface.h"
+#include "util/delay.h"
 
-void CLCD_voidinit(void) {
 
+void CLCD_voidInit(void)
+{
+	/*	SELECT ALL PINS OF LCD OUTPUT */
+	DIO_u8Set_Port_Dir( CLCD_DATA_PORT , DIO_u8_PORT_OUTPUT );
+	DIO_u8Set_Pin_Dir( CLCD_CONTROL_PORT , CLCD_RS_PIN , DIO_u8_PIN_OUTPUT );
+	DIO_u8Set_Pin_Dir( CLCD_CONTROL_PORT , CLCD_EN_PIN , DIO_u8_PIN_OUTPUT );
+	//DIO_u8Set_Pin_Dir( CLCD_CONTROL_PORT , CLCD_RW_PIN , DIO_u8_PIN_OUTPUT );
+	//DIO_u8Set_Pin_Val( CLCD_CONTROL_PORT , CLCD_RW_PIN , DIO_u8_PIN_LOW	   );
+
+
+	/* INITIALIZAION OF 8Bit-MODE CLCD */
 	_delay_ms(50);
 
-	DIO_Set_Port_Dir(CLCD_DATA_PORT, DIO_u8_PORT_OUTPUT);
-	DIO_Set_Port_Dir(CLCD_CTR_PORT, DIO_u8_PORT_OUTPUT);
-	DIO_Set_Pin_Dir(CLCD_CTR_PORT, CLCD_RS_PIN, DIO_u8_OUTPUT);
-	DIO_Set_Pin_Dir(CLCD_CTR_PORT, CLCD_EN_PIN, DIO_u8_OUTPUT);
-	DIO_Set_Pin_Dir(CLCD_CTR_PORT, CLCD_RW_PIN, DIO_u8_OUTPUT);
-	DIO_Set_Pin_Val(CLCD_CTR_PORT, CLCD_RW_PIN, DIO_u8_LOW);
-	/* Function Set of the LCD */
-	CLCD_voidSend_Command(CLCD_u8_SetFun);
-	_delay_ms(1);
-	CLCD_voidSend_Command(CLCD_u8_Dis_Ctr);
-	_delay_ms(2);
-	CLCD_voidSend_Command(CLCD_u8_ENT_MOD);
-
-
-}
-
-void CLCD_voidSend8b_data(u8 copy_u8data) {
-	DIO_Set_Port_Val(CLCD_DATA_PORT, copy_u8data);
-
-	DIO_Set_Pin_Val(CLCD_CTR_PORT, CLCD_RS_PIN, DIO_u8_HIGH);
-
-	CLCD_voidLatch();
-
-}
-
-void CLCD_voidSend_Command(u8 copy_u8command) {
-
-	DIO_Set_Port_Val(CLCD_DATA_PORT, copy_u8command);
-
-	DIO_Set_Pin_Val(CLCD_CTR_PORT, CLCD_RS_PIN, DIO_u8_LOW);
-
-	CLCD_voidLatch();
-
-}
-
-void CLCD_voidSend_String(s8* copy_u8String) {
-	u8 Local_u8_iterator = 0;
-
-	while (copy_u8String[Local_u8_iterator] != '\0') {
-		CLCD_voidSend8b_data(copy_u8String[Local_u8_iterator]);
-
-		Local_u8_iterator++;
-	}
-
-}
-
-void CLCD_voidSend_num(u16 copy_u16Number) {
-
-	/* First Step to reverse the Number */
-	u16 Local_u16_Reversed = 1;
-	if (copy_u16Number == 0) {
-		CLCD_voidSend8b_data('0');
-	}
-	else {
-		while (copy_u16Number != 0) {
-			/* for Examble copy_u16Number = 1234 --> 1- Local = 0 * 10 + 1234 % 10 (4) = 4    , Copy = 123
-			 * 										 2- Local = 4 * 10 + 123 % 10 (3)  = 43   , Copy = 12
-			 * 										 3- Local = 40 * 10 + 12 % 10 (2)  = 432  , Copy = 1
-			 * 										 4-Local = 432 * 10 + 1 % 10 (1)  = 4321  , Copy = 0
-			 * 										                    END OF WHILE
-			 */
-			Local_u16_Reversed = (Local_u16_Reversed * 10)
-					+ (copy_u16Number % 10);
-			copy_u16Number = copy_u16Number / 10;
-		}
-
-		/* Second Step to Devied the Number and Print it*/
-		while (Local_u16_Reversed != 1) {
-			CLCD_voidSend8b_data((Local_u16_Reversed % 10) + '0');
-			Local_u16_Reversed = Local_u16_Reversed / 10;
-
-		}
-	}
-
-}
-
-void CLCD_voidLatch(void) {
-
-	DIO_Set_Pin_Val(CLCD_CTR_PORT, CLCD_EN_PIN, DIO_u8_HIGH);
-	_delay_ms(1);
-	DIO_Set_Pin_Val(CLCD_CTR_PORT, CLCD_EN_PIN, DIO_u8_LOW);
+	/* 		FUNCTION SET	 */
+	CLCD_voidSendCommand(CLCD_u8SET_FUNCTION);
 	_delay_ms(1);
 
+	/* 	DISPLAY ON/OFF CONTROL 	*/
+	CLCD_voidSendCommand(CLCD_u8DISPLAY_CONTROL);
+	_delay_ms(1);
+
+	/* 		DISPLAY CLEAR 		*/
+	CLCD_voidSendCommand(CLCD_u8DISPLAY_CLEAR);
+	_delay_ms(5);
+
+
+	/* 		ENTRY MODE SET 		*/
+	CLCD_voidSendCommand(CLCD_u8_ENTRY_MODE);
+
+
+
 }
 
-void CLCD_voidCLCD_Clear(void) {
-
-	CLCD_voidSend_Command(CLCD_u8_Clear);
-
-}
-
-void CLCD_u8Set_Position(u8 copy_u8Row ,u8 copy_u8coloum)
+void CLCD_voidSendData(u8 Copy_u8Data)
 {
+	/* 	SEND DATA USING DATA PORY 	 */
+	DIO_u8Set_Port_Val( CLCD_DATA_PORT , Copy_u8Data );
 
-	if(copy_u8Row == CLCD_u8_ROW_01)
+	/* SELECT SENDING DATA IN RS PIN */
+	DIO_u8Set_Pin_Val(CLCD_CONTROL_PORT,CLCD_RS_PIN,DIO_u8_PIN_HIGH);
+
+	/* APPLY FALLING EDGE IN EN PIN */
+	DIO_u8Set_Pin_Val(CLCD_CONTROL_PORT,CLCD_EN_PIN,DIO_u8_PIN_HIGH);
+	_delay_ms(1);
+	DIO_u8Set_Pin_Val(CLCD_CONTROL_PORT,CLCD_EN_PIN,DIO_u8_PIN_LOW);
+	_delay_ms(1);
+
+}
+
+void CLCD_voidSendCommand(u8 Copy_u8Command)
+{
+	/* 	SEND DATA USING DATA PORY 	 */
+	DIO_u8Set_Port_Val( CLCD_DATA_PORT , Copy_u8Command );
+
+	/* SELECT SENDING COMMAND IN RS PIN */
+	DIO_u8Set_Pin_Val(CLCD_CONTROL_PORT,CLCD_RS_PIN,DIO_u8_PIN_LOW);
+
+	/* APPLY FALLING EDGE IN EN PIN */
+	DIO_u8Set_Pin_Val(CLCD_CONTROL_PORT,CLCD_EN_PIN,DIO_u8_PIN_HIGH);
+	_delay_ms(1);
+	DIO_u8Set_Pin_Val(CLCD_CONTROL_PORT,CLCD_EN_PIN,DIO_u8_PIN_LOW);
+	_delay_ms(1);
+
+
+}
+
+void CLCD_voidSendString(u8 *Copy_u8PtrString)
+{
+	u8 Local_u8Iterator = 0 ;
+	while(Copy_u8PtrString[Local_u8Iterator] != '\0')
 	{
-		CLCD_voidSend_Command(CLCD_u8_DDRM_Base + copy_u8coloum );
+		CLCD_voidSendData(Copy_u8PtrString[Local_u8Iterator]);
+		Local_u8Iterator++;
+	}
+
+}
+
+void CLCD_voidSendNum(u64 Copy_u64Number)
+{
+	u64 Local_u64Reversed = 1 ;
+	if ( Copy_u64Number == 0 )
+	{
+		CLCD_voidSendData('0');
 	}
 	else
 	{
-		CLCD_voidSend_Command(CLCD_u8_DDRM_Base + copy_u8coloum + CLCD_u8_DDRM_ROW2_OFFSET);
+	/* 		FIRST WE SHOULD REVERSE THE NUMBER 		*/
+	while (Copy_u64Number)
+	{
+		Local_u64Reversed = (Local_u64Reversed*10) + (Copy_u64Number%10);
+		Copy_u64Number = Copy_u64Number / 10 ;
+	}
+	/* 		SECOND WE SHOULD PRINT THE REVERSED NUMBER	*/
+	while (	Local_u64Reversed != 1)
+	{
+		CLCD_voidSendData( (Local_u64Reversed % 10 ) + '0' );
+		Local_u64Reversed /= 10 ;
+	}
+	}
+}
+
+void CLCD_voidSetPosition(u8 Copy_u8Row , u8 Copy_u8Coloum	){
+
+	if(Copy_u8Row == CLCD_u8_ROW_00){
+		CLCD_voidSendCommand(CLCD_u8_DDRAM_OFFSET + Copy_u8Coloum);
+
+	}
+	else{
+		CLCD_voidSendCommand(CLCD_u8_DDRAM_OFFSET + CLCD_u8_DDRAM_ROW2_OFFSET + Copy_u8Coloum);
 	}
 
 
 }
 
-void CLCD_voidSendEXTR_Char()
+void CLCD_voidClearLCD(void)
 {
-	u8 Local_u8Iterator = 0 ;
-	/* First Step is going to CGRAM */
-	CLCD_voidSend_Command(CLCD_u8_CGRAM_ADD);
-
-	/* First Step is Entering the Sympol */
-	for (Local_u8Iterator = 0 ;Local_u8Iterator < (sizeof(Loc_u8StaArr)/sizeof(Loc_u8StaArr[0])) ; Local_u8Iterator++ )
-	{
-		CLCD_voidSend8b_data(Loc_u8StaArr[Local_u8Iterator]);
-	}
-
+	CLCD_voidSendCommand(CLCD_u8_CLEAR_LCD_COMMAND);
 }
