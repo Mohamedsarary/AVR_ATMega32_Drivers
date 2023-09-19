8ba6227675a1811212de4b76273ea354d6e23707
Mohamedsarary <Sarary221@gmail.com>
2023-09-19 11:18:58 +0300

    Edit ADC Driver

--- MCAL/4-ADC/ADC_Private.h
+++ MCAL/4-ADC/ADC_Private.h
@@ -1,47 +1,50 @@
 /*
- *  	Created on: 18/05/2023
- *      Driver: ADC Driver
+ *  	Created on: 18/09/2023
+ *      LAYER : MCAL
  *      Author: Mohamed sarary
  *      File  : ADC_Private.h
  */
 
-#include "../../SERVICES/STD_TYPES.h"
 #ifndef MCAL_ADC_ADC_PRIVATE_H_
 #define MCAL_ADC_ADC_PRIVATE_H_
 
 
-#define ADCL	*((u8 volatile*)(0x24))
-#define ADCH	*((u8 volatile*)(0x25))
-#define ADCSRA	*((u8 volatile*)(0x26))
-#define ADMUX 	*((u8 volatile*)(0x27))
-#define SFIOR	*((u8 volatile*)(0x50))
+/********************** ADC REGISTERS ***********************/
+
+#define ADMUX				*((volatile u8*)(0x27))
+#define ADCSRA				*((volatile u8*)(0x26))
+#define ADCH				*((volatile u8*)(0x25))
+#define ADCL				*((volatile u8*)(0x24))
+#define ADC					*((volatile u16*)(0x24))
+#define SFIOR				*((volatile u8*)(0x50))
 
 
-/*   	 Register ADMUX BITS 		*/
-#define MUX0	0
-#define MUX1	1
-#define MUX2	2
-#define MUX3	3
-#define MUX4	4
-#define ADLAR 	5
-#define REFS0	6
-#define REFS1	7
-
-/*   	 Register ADCSRA BITS 		*/
-#define ADPS0	0
-#define ADPS1	1
-#define ADPS2	2
-#define ADIE	3
-#define ADIF	4
-#define ADATE 	5
-#define ADSC	6
-#define ADEN	7
+/***************** ADMUX REGISTER BITS *****************/
+#define ADMUX_ADLAR				5
+#define ADMUX_REFS0				6
+#define ADMUX_REFS1				7
 
 
-/*   	 Register SFIOR BITS 		*/
-#define ADTS0	5
-#define ADTS1	6
-#define ADTS2	7
+/***************** ADCSRA REGISTER BITS *****************/
+#define ADCSRA_ADPS0			0
+#define ADCSRA_ADPS1			1
+#define ADCSRA_ADPS2			2
+#define ADCSRA_ADIE				3
+#define ADCSRA_ADIF				4
+#define ADCSRA_ADATE			5
+#define ADCSRA_ADSC				6
+#define ADCSRA_ADEN				7
 
+
+/***************** SFIOR REGISTER BITS *****************/
+#define SFIOR_ADTS0				5
+#define SFIOR_ADTS1				6
+#define SFIOR_ADTS2				7
+
+#define NULL					0
+
+static void (*ADC_CallBack) (void) = NULL ;
+
+void __vector_16 (void) __attribute__ ((signal)) ;
 
 #endif /* MCAL_ADC_ADC_PRIVATE_H_ */
