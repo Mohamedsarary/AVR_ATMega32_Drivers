a748c435347261c715bed4cd630f6531f23b6085
Mohamedsarary <Sarary221@gmail.com>
2023-09-25 00:39:04 +0300

    Add TIMER Driver

---
+++ MCAL/5-TIMER/TIMER_Interface.h
@@ -0,0 +1,187 @@
+/*
+ *  	Created on: 24/09/2023
+ *      LAYER : MCAL
+ *      Author: Mohamed sarary
+ *      File  : TIMER_Interface.h
+ */
+
+#ifndef MCAL_TIMER_TIMER_INTERFACE_H_
+#define MCAL_TIMER_TIMER_INTERFACE_H_
+
+#include "../../SERVICES/BitMath.h"
+
+#define TIMER_u8_TIMER_0	0
+#define TIMER_u8_TIMER_1	1
+#define TIMER_u8_TIMER_2	2
+
+
+
+
+/*************************************************************************************************************/
+/********************************************** TIMER 0 INTERFACES *******************************************/
+/*************************************************************************************************************/
+#define TIMER0_u8_NORMAL				0
+#define TIMER0_u8_PWM_PHASE_CORRECT		1
+#define TIMER0_u8_CTC					2
+#define TIMER0_u8_FAST_PWM				3
+
+#define TIMER0_u8_OC0_DISCONNECTED		0
+#define TIMER0_u8_OC0_TOGGLE			1
+#define TIMER0_u8_OC0_CLEAR				2
+#define TIMER0_u8_OC0_SET				3
+
+#define	TIMER0_u8_STOP					0
+#define	TIMER0_u8_DIV_BY_1				1
+#define TIMER0_u8_DIV_BY_8				2
+#define TIMER0_u8_DIV_BY_64				3
+#define TIMER0_u8_DIV_BY_256			4
+#define TIMER0_u8_DIV_BY_1024			5
+
+#define	TIMER0_u8_DISABLE_INTERRUPT		0
+#define	TIMER0_u8_ENABLE_INTERRUPT		1
+
+
+/*************************************************************************************************************/
+/********************************************** TIMER 1 INTERFACES *******************************************/
+/*************************************************************************************************************/
+#define TIMER1_u8_NORMAL								0
+#define TIMER1_u8_PWM_PHASE_CORRECT_8BIT				1
+#define TIMER1_u8_PWM_PHASE_CORRECT_9BIT				2
+#define TIMER1_u8_PWM_PHASE_CORRECT_10BIT				3
+#define TIMER1_u8_CTC_OCR1A								4
+#define TIMER1_u8_FAST_PWM_8BIT							5
+#define TIMER1_u8_FAST_PWM_9BIT							6
+#define TIMER1_u8_FAST_PWM_10BIT						7
+#define TIMER1_u8_PWM_PHASE_FREQUANCY_CORRECT_ICR1		8
+#define TIMER1_u8_PWM_PHASE_FREQUANCY_CORRECT_OCR1A		9
+#define TIMER1_u8_PWM_PHASE_CORRECT_ICR1				10
+#define TIMER1_u8_PWM_PHASE_CORRECT_OCR1A				11
+#define TIMER1_u8_CTC_ICR1								12
+#define TIMER1_u8_FAST_PWM_ICR1							13
+#define TIMER1_u8_FAST_PWM_OCR1A						14
+
+#define TIMER1_u8_OC1_DISCONNECTED			0
+#define TIMER1_u8_OC1_TOGGLE				1
+#define TIMER1_u8_OC1_CLEAR					2
+#define TIMER1_u8_OC1_SET					3
+
+#define	TIMER1_u8_STOP						0
+#define	TIMER1_u8_DIV_BY_1					1
+#define TIMER1_u8_DIV_BY_8					2
+#define TIMER1_u8_DIV_BY_64					3
+#define TIMER1_u8_DIV_BY_256				4
+#define TIMER1_u8_DIV_BY_1024				5
+
+
+#define	TIMER1_u8_FALLING_INPUT_CAPTURE		0
+#define	TIMER1_u8_RISING_INPUT_CAPTURE		1
+
+
+/*************************************************************************************************************/
+/********************************************** TIMER 2 INTERFACES *******************************************/
+/*************************************************************************************************************/
+#define TIMER2_u8_NORMAL				0
+#define TIMER2_u8_PWM_PHASE_CORRECT		1
+#define TIMER2_u8_CTC					2
+#define TIMER2_u8_FAST_PWM				3
+
+#define TIMER2_u8_OC2_DISCONNECTED		0
+#define TIMER2_u8_OC2_TOGGLE			1
+#define TIMER2_u8_OC2_CLEAR				2
+#define TIMER2_u8_OC2_SET				3
+
+#define	TIMER2_u8_STOP					0
+#define	TIMER2_u8_DIV_BY_1				1
+#define TIMER2_u8_DIV_BY_8				2
+#define TIMER2_u8_DIV_BY_32				3
+#define TIMER2_u8_DIV_BY_64				4
+#define TIMER2_u8_DIV_BY_128			5
+#define TIMER2_u8_DIV_BY_256			6
+#define TIMER2_u8_DIV_BY_1024			7
+
+#define	TIMER2_u8_DISABLE_INTERRUPT		0
+#define	TIMER2_u8_ENABLE_INTERRUPT		1
+
+
+void TIMER_voidInit(void);
+
+/*************************************************************************************************************/
+/***************************************** TIMER 0 FUNCTIONS PROTOTYPE ***************************************/
+/*************************************************************************************************************/
+
+void TIMER0_voidSetOVFRegister(u8 Copy_u8Value);
+void TIMER0_voidSetCTCRegister(u8 Copy_u8Value);
+
+void TIMER0_voidEnableOVFINT(void);
+void TIMER0_voidDisableOVFINT(void);
+void TIMER0_voidClearOVFFlag(void);
+
+void TIMER0_voidEnableCTCINT(void);
+void TIMER0_voidDisableCTCINT(void);
+void TIMER0_voidClearCTCFlag(void);
+
+void TIMER0_voidSetOVFCallBack(void (*Copy_pvoidCallBack) (void));
+void TIMER0_voidSetCTCCallBack(void (*Copy_pvoidCallBack) (void));
+void TIMER0_voidForceOutputCompare(void);
+
+
+/*************************************************************************************************************/
+/***************************************** TIMER 1 FUNCTIONS PROTOTYPE ***************************************/
+/*************************************************************************************************************/
+
+void TIMER1_voidSetOVFRegister(u16 Copy_u16Value);
+void TIMER1_voidChannelASetCTCRegister(u16 Copy_u16Value);
+void TIMER1_voidChannelBSetCTCRegister(u16 Copy_u16Value);
+u16	 TIMER1_u16InputCaptureValue(void);
+
+
+void TIMER1_voidEnableOVFINT(void);
+void TIMER1_voidDisableOVFINT(void);
+void TIMER1_voidClearOVFFlag(void);
+
+
+void TIMER1_voidChannelAEnableCTCINT(void);
+void TIMER1_voidChannelADisableCTCINT(void);
+void TIMER1_voidChannelAClearCTCFlag(void);
+
+void TIMER1_voidChannelBEnableCTCINT(void);
+void TIMER1_voidChannelBDisableCTCINT(void);
+void TIMER1_voidChannelBClearCTCFlag(void);
+
+void TIMER1_voidEnableInputCaptureINT(void);
+void TIMER1_voidDisableInputCaptureINT(void);
+void TIMER1_voidClearInputCaptureFlag(void);
+
+void TIMER1_voidSetOVFCallBack(void (*Copy_pvoidCallBack) (void));
+void TIMER1_voidChannelASetCTCCallBack(void (*Copy_pvoidCallBack) (void));
+void TIMER1_voidChannelBSetCTCCallBack(void (*Copy_pvoidCallBack) (void));
+void TIMER1_voidSetInputCaptureCallBack(void (*Copy_pvoidCallBack) (void));
+
+void TIMER1_voidChannelAForceOutputCompare(void);
+void TIMER1_voidChannelBForceOutputCompare(void);
+
+
+
+/*************************************************************************************************************/
+/***************************************** TIMER 2 FUNCTIONS PROTOTYPE ***************************************/
+/*************************************************************************************************************/
+
+void TIMER2_voidSetOVFRegister(u8 Copy_u8Value);
+void TIMER2_voidSetCTCRegister(u8 Copy_u8Value);
+
+void TIMER2_voidEnableOVFINT(void);
+void TIMER2_voidDisableOVFINT(void);
+void TIMER2_voidClearOVFFlag(void);
+
+void TIMER2_voidEnableCTCINT(void);
+void TIMER2_voidDisableCTCINT(void);
+void TIMER2_voidClearCTCFlag(void);
+
+void TIMER2_voidSetOVFCallBack(void (*Copy_pvoidCallBack) (void));
+void TIMER2_voidSetCTCCallBack(void (*Copy_pvoidCallBack) (void));
+
+void TIMER2_voidForceOutputCompare(void);
+
+
+
+#endif /* MCAL_TIMER_TIMER_INTERFACE_H_ */
---
+++ MCAL/5-TIMER/TIMER_Private.h
@@ -0,0 +1,150 @@
+/*
+ *  	Created on: 24/09/2023
+ *      LAYER : MCAL
+ *      Author: Mohamed sarary
+ *      File  : TIMER_Private.h
+ */
+
+#ifndef MCAL_TIMER_TIMER_PRIVATE_H_
+#define MCAL_TIMER_TIMER_PRIVATE_H_
+
+#include "../../SERVICES/StdTypes.h"
+
+#define NULL				0
+
+/*************************************************************************************************************/
+/************************************ TIMERS REGISTER ADDRESSES **********************************************/
+/*************************************************************************************************************/
+
+/* TIMERS COMMON REGISTERS */
+#define TIMSK	*((volatile u8*)(0x59))
+#define TIFR	*((volatile u8*)(0x59))
+
+/* TIMER 0 REGISTERS */
+#define TCCR0	*((volatile u8*)(0x53))
+#define TCNT0   *((volatile u8*)(0x52))
+#define OCR0	*((volatile u8*)(0x5C))
+
+/* TIMER 1 REGISTERS */
+#define TCCR1A  *((volatile u8*)(0x4F))
+#define TCCR1B  *((volatile u8*)(0x4E))
+#define TCNT1   *((volatile u8*)(0x4C))
+#define OCR1A   *((volatile u8*)(0x4A))
+#define OCR1B   *((volatile u8*)(0x48))
+#define ICR1    *((volatile u8*)(0x46))
+
+/* TIMER 2 REGISTERS */
+#define TCCR2   *((volatile u8*)(0x45))
+#define TCNT2   *((volatile u8*)(0x44))
+#define OCR2    *((volatile u8*)(0x43))
+
+/*************************************************************************************************************/
+/***************************************** END OF REGISTER ADDRESSES *****************************************/
+/*************************************************************************************************************/
+
+/*************************************************************************************************************/
+/*****************************************  TIMERS REGISTER BITS *********************************************/
+/*************************************************************************************************************/
+
+/* TIMSK REGISTER BITS */
+#define TIMSK_TOIE0		0
+#define TIMSK_OCIE0		1
+#define TIMSK_TOIE1		2
+#define TIMSK_OCIE1B	3
+#define TIMSK_OCIE1A	4
+#define TIMSK_TICIE1	5
+#define TIMSK_TOIE2		6
+#define TIMSK_OCIE2		7
+
+/* TIFR REGISTER BITS */
+#define TIFR_TOV0		0
+#define TIFR_OCF0		1
+#define TIFR_TOV1		2
+#define TIFR_OCF1B		3
+#define TIFR_OCF1A		4
+#define TIFR_ICF1		5
+#define TIFR_TOV2		6
+#define TIFR_OCF2		7
+
+/* TCCR0 REGISTER BITS */
+#define TCCR0_CS00		0
+#define TCCR0_CS01		1
+#define TCCR0_CS02		2
+#define TCCR0_WGM01		3
+#define TCCR0_COM00		4
+#define TCCR0_COM01		5
+#define TCCR0_WGM00		6
+#define TCCR0_FOC0		7
+
+/* TCCR1A REGISTER BITS */
+#define TCCR1A_COM1A1	7
+#define TCCR1A_COM1A0	6
+#define TCCR1A_COM1B1	5
+#define TCCR1A_COM1B0	4
+#define TCCR1A_FOC1A	3
+#define TCCR1A_FOC1B	2
+#define TCCR1A_WGM11	1
+#define TCCR1A_WGM10	0
+
+/* TCCR1B REGISTER BITS */
+#define TCCR1B_ICNC1	7
+#define TCCR1B_ICES1	6
+#define TCCR1B_WGM13	4
+#define TCCR1B_WGM12	3
+#define	TCCR1B_CS12		2
+#define TCCR1B_CS11		1
+#define TCCR1B_CS10		0
+
+/* TIFR REGISTER BITS */
+#define TIFR_ICF1		5
+#define TIFR_OCF1A		4
+#define TIFR_OCF1B		3
+#define TIFR_TOV1		2
+
+/* TCCR2 REGISTER BITS */
+#define	TCCR2_FOC2		7
+#define TCCR2_WGM20		6
+#define TCCR2_COM21		5
+#define TCCR2_COM20		4
+#define TCCR2_WGM21		3
+#define	TCCR2_CS22		2
+#define	TCCR2_CS21		1
+#define	TCCR2_CS20		0
+
+#define TIMSK_TOIE2	6
+#define TIMSK_OCIE2	7
+
+#define TIFR_TOV2	6
+#define TIFR_OCF2	7
+
+
+/*************************************************************************************************************/
+/**************************************  END OF TIMERS REGISTER BITS *****************************************/
+/*************************************************************************************************************/
+
+/*************************************************************************************************************/
+/******************************************** TIMERS ISR VECTOR IDS ******************************************/
+/*************************************************************************************************************/
+void __vector_11 (void) __attribute__ ((signal));		/*	TIMER 0 OVF MODE ISR ID */
+void __vector_10 (void) __attribute__ ((signal));		/*	TIMER 0 CTC MODE ISR ID */
+void __vector_9  (void) __attribute__ ((signal));		/*	TIMER 1 OVF MODE ISR ID */
+void __vector_8  (void) __attribute__ ((signal));		/*	TIMER 1 COMP B MODE ISR ID */
+void __vector_7  (void) __attribute__ ((signal));		/*	TIMER 1 COMP A MODE ISR ID */
+void __vector_6  (void) __attribute__ ((signal));		/*	TIMER 1 CAPT MODE ISR ID */
+void __vector_5  (void) __attribute__ ((signal));		/*	TIMER 2 OVF MODE ISR ID */
+void __vector_4  (void) __attribute__ ((signal));		/*	TIMER 2 CTC MODE ISR ID */
+
+/*************************************************************************************************************/
+/**************************************** END OF TIMERS ISR VECTOR IDS ***************************************/
+/*************************************************************************************************************/
+
+static void (*TIMER0_OVF_PCallBack)(void) = NULL;
+static void (*TIMER0_CTC_PCallBack)(void) = NULL;
+static void (*TIMER1_OVF_PCallBack)(void) = NULL;
+static void (*TIMER1_CTC_CHANNELA_PCallBack)(void) = NULL;
+static void (*TIMER1_CTC_CHANNELB_PCallBack)(void) = NULL;
+static void (*TIMER1_INU_PCallBack)(void) = NULL;
+static void (*TIMER2_OVF_PCallBack)(void) = NULL;
+static void (*TIMER2_CTC_PCallBack)(void) = NULL;
+
+#endif /* MCAL_TIMER_TIMER_PRIVATE_H_ */
